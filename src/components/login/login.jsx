a6c76df4a9aff5cb31d8300903d31ad4f31e6f24
Push <push@Pushs-MacBook-Air.local>
2024-05-13 19:06:45 +0530

    login and footer page upadted

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
-import {Context as context} from "../../shared/context"
+import { Context as context } from "../../shared/context";
 import "./login.css";
 
 export function Login() {
   const auth = context();
-  const [login, setLogin] = useState({username: "", password: "", "error": ""});
+  const [login, setLogin] = useState({ username: "", password: "", error: "" });
 
   const onLogin = () => {
     if (!login.username) {
-      setLogin(prevState => ({
+      setLogin((prevState) => ({
         ...prevState,
-        'error': 'Please fill Username',
+        error: "Please fill Username",
       }));
       return;
     }
     if (!login.password) {
-      setLogin(prevState => ({
+      setLogin((prevState) => ({
         ...prevState,
-        'error': 'Please fill Password',
+        error: "Please fill Password",
       }));
       return;
     }
 
     auth.saveToken(login).then((data) => {
       debugger;
-      if (data && data.status === 'mfa') {
+      if (data && data.status === "mfa") {
         auth.setState((prevState) => ({
           ...prevState,
-          error: '',
-          success: '',
+          error: "",
+          success: "",
           loading: false,
           secure: {
             hash: data.hash,
@@ -40,16 +40,16 @@ export function Login() {
       } else {
         auth.setState((prevState) => ({
           ...prevState,
-          error: 'Invalid username or password',
-          success: '',
+          error: "Invalid username or password",
+          success: "",
           loading: false,
         }));
       }
     });
-  }
+  };
 
   const handleInput = (name) => (e) => {
-    setLogin(prevState => ({
+    setLogin((prevState) => ({
       ...prevState,
       [name]: e.target.value,
     }));
@@ -57,22 +57,19 @@ export function Login() {
 
   return (
     <>
-      <form onSubmit={onLogin} className="loginForm">
-        <div className="form-container">
-          {login.error && <div>{login.error}</div>}
-          <div className="form-group">
-            <label htmlFor="uname">Username:</label>
-            <input type="text" onChange={handleInput('username')} className="form-control" id="uname" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="pwd">Password:</label>
-            <input type="password" onChange={handleInput('password')} className="form-control" id="pwd" />
-          </div>
-          <button type="submit" style={{color:'#fff'}} className="btn btn-default" >
+      <div className="log-form">
+        <div className="logo">
+          <img src="src/assets/logo.png" alt="" />
+        </div>
+        <h2>Login to your account</h2>
+        <form>
+          <input type="text" title="username" placeholder="Username" />
+          <input type="password" title="username" placeholder="Password" />
+          <button type="submit" className="btn">
             Login
           </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
